1b47368bdaa0b37c3f68691c40e99f4c77a53a06
Dylan Becker <dbecker@lcs-mba3263.lcs.on.ca>
2017-10-16 09:41:34 -0400

    Random  circles whithout fail safe  (2 or more colours in the same circle)

--- IterationExamples/Iteration Examples.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
+++ IterationExamples/Iteration Examples.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
@@ -12,10 +12,26 @@ import PlaygroundSupport
 //: ## Add your code below
 
 // Create canvas
-let canvas = Canvas(width: 300, height: 300)
+let canvas = Canvas(width: 600, height: 600)
+
+//Defaults
+canvas.defaultLineWidth = 1
 
 // Below this line, try combining a loop and four statements that draw lines to generate the goal
 
+for x in stride(from: 50, through: 550, by: 100){
+    for y in stride(from: 50, through: 550, by: 100){
+        for d in stride(from: 100, through: 20, by: -20){
+        var v = random(from: 0, toButNotIncluding: 361)
+            canvas.fillColor = Color.init(hue: v, saturation: 100, brightness: 100, alpha: 100)
+            canvas.borderColor = Color.init(hue: v, saturation: 100, brightness: 100, alpha: 100)
+
+            canvas.drawEllipse(centreX: x, centreY: y, width: d, height: d)
+            
+            
+        }
+    }
+}
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
